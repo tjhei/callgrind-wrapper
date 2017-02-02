6059d52d3baef7b5724ebe96a358cdfbd84d9118
Timo Heister <timo.heister@gmail.com>
2017-02-02 15:34:31 -0500

    nicely working

--- perf.h
+++ perf.h
@@ -4,16 +4,106 @@
 /*
 Usage:
 
-valgrind --tool=callgrind -q --combine-dumps=yes --callgrind-out-file=callgrind.out
+valgrind --tool=callgrind -q --combine-dumps=yes --callgrind-out-file=callgrind.out ./executable
 
  */
-
 
 #include <valgrind/callgrind.h>
 #include <stdlib.h>
 #include <fstream>
 #include <iostream>
+#include <cstdio>
 
+#include <sys/types.h>       
+#include <unistd.h>
+
+namespace callgrind_wrapper
+{
+  
+
+/**
+ * reset the current instruction counter to zero and start instrumenting (if
+ * not already happening).
+ */
+inline void reset()
+{
+	CALLGRIND_START_INSTRUMENTATION;
+	CALLGRIND_ZERO_STATS;
+}
+
+/**
+ * Return the number of instructions since reset() has been called last. This
+ * might return 0 or an invalid number if the program is not executed under
+ * callgrind with the recommended parameters.
+ */
+inline long current()
+{
+  CALLGRIND_DUMP_STATS;
+  std::ifstream f("callgrind.out");
+  std::string bla;
+  
+  long cycles = 0;
+  while (f)
+    {
+      f >> bla;
+      if (bla == "summary:")
+	f >> cycles;	    
+    }
+  return cycles;
+}
+
+/**
+ * A somewhat expensive call to check that we are running under callgrind,
+ * that the parameters are set correctly, and that we can retrieve cycle
+ * information.
+ */
+inline bool is_working()
+{
+  int running = RUNNING_ON_VALGRIND;
+  if (running==0)
+    return false;
+
+  reset();
+  CALLGRIND_DUMP_STATS;
+  
+  std::string token = "callgrind-wrapper-token";
+  CALLGRIND_DUMP_STATS_AT(token.c_str());
+  
+  std::ifstream f("callgrind.out");
+  std::string bla;
+  long cycles = 0;
+  
+  bool found_token = false;
+  pid_t mypid = getpid();
+  long found_pid = 0;
+  
+  while (f)
+    {
+      f >> bla;
+      if (bla == "pid:")
+	f >> found_pid;
+      if (bla == "desc:")
+	{
+	  
+	  std::string line;
+	  std::getline(f, line);
+	  if (line == " Trigger: Client Request: " + token)
+	    found_token = true; 
+	}
+      if (bla == "summary:")
+	f >> cycles;
+    }
+  
+  if (!found_token || found_pid != mypid)
+    return false;
+  
+  return cycles > 0;
+}
+
+/**
+ * A simple class that logs cycle information to std::cout when going out of
+ * scope.
+ */
 class Instrument
 {
   public:
@@ -22,39 +112,27 @@ class Instrument
 		    name (name),
 		    stopped(false)
       {
-	CALLGRIND_START_INSTRUMENTATION;
-	CALLGRIND_ZERO_STATS;
+	reset();
       }
 
     void stop()
       {
-	if (stopped)
-	  return;
-	
+	long cycles = current();
 	stopped = true;
-	int ret;
-	//ret = system("rm -f log*");
-	CALLGRIND_DUMP_STATS_AT(name);
 	
-	ret = system("cat log* | grep summary >log.summary");
-
-	std::ifstream f("log.summary");
-	std::string bla;
-	long cycles = 0;
-	f >> bla >> cycles;
 	std::cout << "> " << name << " " << cycles << std::endl;
-	
-	//ret = system("rm -f log*");
       }
 
     ~Instrument()
       {
-	stop();
+	if (!stopped)
+	  stop();
       }    
 
   private:
     const char* name;
     bool stopped;
 };
+}
 
 #endif
---
+++ test.cc
@@ -0,0 +1,27 @@
+#include "perf.h"
+#include <iostream>
+
+int main()
+{
+  if (!callgrind_wrapper::is_working())
+    {
+      std::cout << "callgrind not detected. Please run as\n"
+		<< "    valgrind --tool=callgrind -q --combine-dumps=yes --callgrind-out-file=callgrind.out ./a.out"<< std::endl;
+      return -1;
+    }
+  std::cout << "running correctly under callgrind..." << std::endl;
+  
+  {
+    callgrind_wrapper::Instrument a("nothing");
+  }
+  {
+    callgrind_wrapper::Instrument a("something");
+    int t = 0;
+    for (unsigned int i=0;i<1000;++i)
+      for (unsigned int j=0;j<1000;++j)
+	t =+ (i+j)%10;
+    std::cout << "computing 8 = " <<  t << std::endl;
+  }
+  
+
+}
